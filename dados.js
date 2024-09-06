e9653fb8976dec17cce501b2572621c315a237ba
Larissa Cassanjo <155493156+Lari-Cassanjo@users.noreply.github.com>
2024-09-06 10:52:44 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,26 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao:
+            "Rayssa Leal é uma skatista brasileira que conquistou o mundo com seu talento precoce. Conhecida como 'Fadinha', ela é um dos maiores nomes do skate feminino.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        redessociais: "https://www.instagram.com/rayssalealsk8/",
+        tags: "skate sk8",
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao:
+            "Rebeca Andrade é uma ginasta brasileira que conquistou diversas medalhas em competições internacionais, incluindo o ouro olímpico. Conhecida por sua força e elegância, ela é um dos principais nomes da ginástica artística mundial.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        redessociais: "https://www.instagram.com/rebecarandrade/",
+        tags: "ouro simone ginástica ginastica",
+    },
+    {
+        titulo: "Flávia Saraiva",
+        descricao:
+            "Flávia Saraiva é uma ginasta brasileira que se destacou por sua versatilidade e carisma. Ela conquistou diversas medalhas em competições nacionais e internacionais, sendo uma das principais atletas da ginástica artística brasileira.",
+        link: "https://pt.wikipedia.org/wiki/Flávia_Saraiva",
+        redessociais: "https://www.instagram.com/flavialopessaraiva/",
+        tags: "ginástica ginastica",
+    },
+];
